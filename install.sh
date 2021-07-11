5e5b18e3027f33481c1f1628b72c45205d624529
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-07-12 00:23:49 +0430

    Hack v2.0.0
    Hack
    Version: 2.0.0

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+# Black-Tool Installing
+# Version 2.0.0
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "Please, Run This Programm as Root!"
+  exit 1
+fi 
+printf '\033]2;Installing\a'
+clear
+echo "Installing..."
+sleep 2
+chmod a+x hack
+chmod +x uninstall.sh
+apt install xtitle
+apt install lolcat
+apt install python
+apt install python3
+cp hack /usr/bin
+printf "Press any key To continue..."
+read -n 1
+echo ""
+echo "Finish...!
+
+Usage:
+       hack --help
+       "
+echo ""
+exit 1
